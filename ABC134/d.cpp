82cb5f2ba589309bfa762ce9c14a5ae5cde3d41c
KoukiNAGATA <koukinagata.academic02@gmail.com>
2020-04-27 21:14:31 +0900

    abc134d

--- ABC134/d.cpp
+++ ABC134/d.cpp
@@ -1,34 +1,49 @@
 #include<iostream>
 #include<vector>
-#include<cstdio>
 #include<string>
+#include<map>
+#include<utility>
 #include<algorithm>
+#include<queue>
+#include<cmath>
+#include<iomanip>
+#define REP(i, n) for(ll i = 0; i < n; i++)
+#define REPR(i, n) for(ll i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for(ll i = m; i <= n; i++)
+#define FORR(i, m, n) for(ll i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v+n)
+#define MAX 100000
+#define inf 1000000007
 using namespace std;
-#define MAX 1000000000
-
-long long GCD(long long x, long long y) {//最大公約数
-    //ユークリッド：x%y!=0ならばGCD(y, x%y)、0ならばyを返す(x>y)
-    return x%y ? GCD(y, x%y) : y;
-}
-
-long long LCM(long long x, long long y) {//最小公倍数
-    return x * y / GCD(x, y);
-}
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll, ll>;
 
 int main() {
-    long long a, b;
-    int ans = 1;
-    cin >> a >> b;
-    long long gcd = GCD(a, b);
-    //GCDの約数のなかで素数であるものを探す
-    for(int i = 2; i <= gcd; i++){//gcdも変化していく
-        if (gcd % i == 0) {
-			ans ++;
-		}
-        while (gcd % i == 0) { // 素数で割り切れなくなるまで割っていく
-			gcd /= i;
-		}
+    //cin高速化
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    ll n, m = 0;
+    cin >> n;
+    //1オリジン
+    vll a(n+1, 0);//初期化忘れない
+    vll b(n+1, 0);
+    FOR(i, 1, n) cin >> a[i];
+    FORR(i, n, 1){
+        ll tmp = 0;
+        for(ll j = i; j <= n; j += i){//倍数だけチェック
+            tmp += b[j];
+        }
+        if(tmp%2 != a[i]){
+            b[i] = 1;
+            m ++;
+        }
     }
-    cout << ans <<endl;
-    return 0;
+
+    cout << m << endl;
+    FOR(i, 1, n){
+        if(b[i] == 1) cout << i << " " << endl;
+    }
+	return 0;
 }
