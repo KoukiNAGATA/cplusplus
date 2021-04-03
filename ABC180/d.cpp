3f295e178400a00257bdebb84bb603d12e55b154
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-04-03 15:59:22 +0900

    0403

--- ABC180/d.cpp
+++ ABC180/d.cpp
@@ -1,4 +1,3 @@
-// 二分探索
 #include <bits/stdc++.h>
 #define REP(i, n) for (int i = 0; i < n; i++)
 #define REPR(i, n) for (int i = n - 1; i >= 0; i--)
@@ -15,31 +14,21 @@ using Edge = pair<int, ll>;
 const int MAX = 100000;
 const ll INF = 1LL << 60;
 
-ll a, b, x;
-bool check(ll n)
-{
-    if (a * n + b * to_string(n).size() <= x)
-        return true;
-    else
-        return false;
-}
-
 int main()
 {
     // cin高速化
     cin.tie(0);
     ios::sync_with_stdio(false);
-    cin >> a >> b >> x;
-    ll l = 0, r = 1e9 + 1;
-    // 二分探索
-    while (r - l > 1)
+    ll x, y, a, b, ans;
+    cin >> x >> y >> a >> b;
+    ans = 0;
+    // x = ax
+    while (x < y / a && a * x <= x + b)
     {
-        ll mid = (r + l) / 2;
-        if (check(mid)) // 買える
-            l = mid;
-        else // 買えない
-            r = mid;
+        x *= a;
+        ans++;
     }
-    cout << l << "\n";
+    // x = x + b
+    cout << ans + (y - 1 - x) / b << "\n";
     return 0;
 }
