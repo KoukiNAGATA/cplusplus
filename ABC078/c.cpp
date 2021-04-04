0b4107117cdabd5bec26bd74b35841defbd2a8e8
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-04-04 19:50:31 +0900

    0404

---
+++ ABC078/c.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n)
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll, ll>;
+using Graph = vector<vector<int>>;
+using Edge = pair<int, ll>;
+const int MAX = 100000;
+const ll INF = 1LL << 60;
+
+int main()
+{
+    // cin高速化
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    ll n, m;
+    cin >> n >> m;
+    // 提出回数の期待値
+    ll times = 1LL << m;
+    // 1回あたりの提出
+    ll ans = 1900 * m + 100 * (n - m);
+    cout << ans * times << "\n";
+    return 0;
+}
