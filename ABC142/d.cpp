8ef1d2b6e55058355765197213d22167ec0834a9
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-06-05 02:25:54 +0900

    0605

--- ABC142/d.cpp
+++ ABC142/d.cpp
@@ -1,10 +1,19 @@
-#include <iostream>
-#include <vector>
-#include <cstdio>
-#include <string>
-#include <algorithm>
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n)
 using namespace std;
 using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll, ll>;
+using Graph = vector<vector<int>>;
+using Edge = pair<int, ll>;
+const ll INF = 1LL << 60;
+const int MAX = 100000;
+const int MOD = 1000000007;
 
 ll GCD(ll x, ll y)
 { //最大公約数
@@ -32,20 +41,21 @@ int main()
     if (gcd > 1)
     {
         for (ll i = 2; i * i <= gcd; i++)
-        { //gcdも変化していく
+        {
             if (gcd % i == 0)
             {
                 ans++;
                 while (gcd % i == 0)
-                { // 素数で割り切れなくなるまで割っていく
+                { // その素数で割り切れなくなるまで割っていく
                     gcd /= i;
                 }
             }
         }
     }
 
+    // gcdが素数
     if (gcd > 1)
-        ans++; //i < gcd(iより大きい素数の積)ならループは続くし、i > gcd(iより小さい素数)なら後は1つの素数でgcdを割ったら終わり。1足すのと同じ
+        ans++;
 
     cout << ans << "\n";
     return 0;
