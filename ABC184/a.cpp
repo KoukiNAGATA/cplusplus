292d40cf787b22e321ecb8e88e9bd4c414cf075d
KoukiNAGATA <koukinagata.academic02@gmail.com>
2021-06-10 23:53:40 +0900

    0610

---
+++ ABC184/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for (int i = 0; i < n; i++)
+#define REPR(i, n) for (int i = n - 1; i >= 0; i--)
+#define FOR(i, m, n) for (int i = m; i <= n; i++)
+#define FORR(i, m, n) for (int i = m; i >= n; i--)
+#define SORT(v, n) sort(v, v + n)
+using namespace std;
+using ll = long long;
+using vll = vector<ll>;
+using vvll = vector<vector<ll>>;
+using P = pair<ll, ll>;
+using Graph = vector<vector<int>>;
+using Edge = pair<int, ll>;
+const ll INF = 1LL << 60;
+const int MAX = 200000;
+const int MOD = 1000000007;
+
+int main()
+{
+    // cin高速化
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << a * d - b * c << endl;
+}
